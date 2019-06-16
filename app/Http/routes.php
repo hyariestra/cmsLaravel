a770633517e44b1475e63942e42bcb699bee5df9
hyariestra <yudhaariestra@gmail.com>
2019-06-17 00:11:21 +0700

    comment and make post view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,6 +20,8 @@ Route::auth();
 Route::get('/home', 'HomeController@index');
   
 
+Route::get('/post/{id}', ['as'=>'home.post','uses'=>'AdminPostsController@post']);
+
 
 Route::group(['middleware'=>'admin'],function(){
 
@@ -33,5 +35,7 @@ Route::resource('admin/posts','AdminPostsController');
 Route::resource('admin/categories','AdminCategoriesController');
 Route::resource('admin/media','AdminMediasController');
 
+Route::resource('admin/comments','PostCommentsController');
+Route::resource('admin/comment/replies','CommentRepliesController');
 
 });
